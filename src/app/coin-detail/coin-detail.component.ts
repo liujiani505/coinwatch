633ee5442036014d20af1d3e2ce1dabfa8088475
Jiani Liu <liujiani505@gmail.com>
2022-12-29 23:33:02 -0500

    finishing the chart

--- src/app/coin-detail/coin-detail.component.ts
+++ src/app/coin-detail/coin-detail.component.ts
@@ -21,11 +21,13 @@ export class CoinDetailComponent implements OnInit {
       {
         data:[],
         label: 'Price Trends',
-        backgroundColor: 'rgba(148,159,177,0.2)',
-        pointBackgroundColor:'#009688',
-        pointBorderColor: '#009688',
-        pointHoverBackgroundColor: '#009688',
-        pointHoverBorderColor: '#009688',
+        backgroundColor: '#B7F200',
+        pointBackgroundColor:'#B7F200',
+        pointBorderColor: '#B7F200',
+        pointHoverBackgroundColor: '#B7F2008',
+        pointHoverBorderColor: '#B7F200',
+        borderColor: '#B7F200',
+
       }
     ],
     labels:[]
@@ -66,7 +68,18 @@ export class CoinDetailComponent implements OnInit {
   getGraphData(){
     this.api.getGraphicalCurrencyData(this.coinId, "USD", 1)
     .subscribe(res => {
+      setTimeout(()=>{
+        this.lineChart.chart?.update();
+      }, 200)
       console.log(res)
+      this.lineChartData.datasets[0].data = res.prices.map((a:any) => {
+        return a[1];
+      });
+      this.lineChartData.labels = res.prices.map((a:any) => {
+        let date = new Date(a[0]);
+        let time = date.getHours() > 12 ? `${date.getHours() -12} : ${date.getMinutes()} PM` : `${date.getHours()} : ${date.getMinutes()} AM`
+        return this.days === 1 ? time : date.toLocaleDateString();
+      })
     })
   }
 
