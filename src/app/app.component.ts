ffc7f9922b1300567fa0cc43961408d730974c53
Jiani Liu <liujiani505@gmail.com>
2023-01-01 23:16:42 -0500

    change currency in home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CurrencyService } from './service/currency.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,10 @@ export class AppComponent {
   title: string = "coinwatch";
   selectedcurrency: string ='USD';
 
-  constructor(){}
+  constructor(private currencyService: CurrencyService){}
 
   sendCurrency(event:string){
     console.log(event);
+    this.currencyService.setCurrency(event)
   }
 }
